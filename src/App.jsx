b7d0020e39b3580d0c4c994b64c8d503caf8c84c
BJoarder <bjoarder87@gmail.com>
2023-04-09 22:26:25 +0600

    project all setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h2 className='bg-purple-500 text-center'>Hello react router assignment</h2>
+    </div>
+  )
+}
+
+export default App
