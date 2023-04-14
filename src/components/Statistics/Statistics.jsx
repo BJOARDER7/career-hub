1c79acbe0073eb70d0822b8570e72de40bb0835b
BJoarder <bjoarder87@gmail.com>
2023-04-14 23:24:01 +0600

    Rechart added & final commit

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts';
 
 const Statistics = () => {
+    
+  const data = [
+    { name: 'Assignment-1', value: 60 },
+    { name: 'Assignment-2', value: 60 },
+    { name: 'Assignment-3', value: 30 },
+    { name: 'Assignment-4', value: 53 },
+    { name: 'Assignment-5', value: 60 },
+    { name: 'Assignment-6', value: 56 },
+    { name: 'Assignment-7', value: 60 },
+    
+  ];
+  
+   
+    
+      return (
+        <div style={{ width: '100%', height: 300 }}>
+          <ResponsiveContainer>
+            <PieChart title='This is my assignment marks'>
+              <Pie dataKey="value" data={data} fill="#8884d8" label />
+            </PieChart>
+            </ResponsiveContainer>
+        </div>
+      );
+    };
+
 
   
-  return (
-    <div>
-      <h2>This is statistics page</h2>
-    </div>
-  );
-};
+
 
 export default Statistics;
